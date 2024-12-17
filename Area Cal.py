d041c0cb7011b91e91f5dd006c68ef8fd0635866
Jack R Stylianou <63659555+Jackona@users.noreply.github.com>
2024-12-17 10:58:59 +0000

    Add files via upload

---
+++ Area Cal.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of a rectangle:"))
+width = float(input("Enter the width of a rectangle:"))
+height = float(input("Enter the height of a rectangle:"))
+
+volume = length * width * height
+print(f"the volume is: {volume}cm^3")
